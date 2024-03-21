dc69b880ca9d6cdcbf8161dcc92a812f4ac36819
madhavan <madhavankca@gmail.com>
2024-03-21 06:20:10 +0530

    Add. alert announce winner

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const AVAILABLEMOVES = 3;
 let moveChoice = ['rock','paper','scissor'];
 function getComputerChoice(){
-    let computerMove = (Math.floor(Math.random()*3));
-    console.log(moveChoice[computerMove]);
+    let computerMove = Math.floor(Math.random()*AVAILABLEMOVES);
+    console.log("Computer choice = " + moveChoice[computerMove]);
+    return moveChoice[computerMove];
 }
-getComputerChoice();
+playerSelection = prompt("Type your move (rock, paper, scissor)").toLowerCase();
+    let computerSelection = getComputerChoice();
+    let result = playgame(playerSelection, computerSelection);
+    alert(result);
+
+function playgame(playerSelection, computerSelection) {
+     console.log("Player choice = " + playerSelection);
+     if(playerSelection == computerSelection) return("It's a Tie");
+     else if((playerSelection === 'rock' && computerSelection === 'scissor') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissor' && computerSelection === 'paper'))
+        return("Player wins! "+ playerSelection+ " beats "+ computerSelection);
+    else{
+     return("Computer wins! "+ computerSelection+ " beats "+ playerSelection);
+    }
+}
